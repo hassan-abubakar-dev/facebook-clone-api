555eabd43ad5973af312d728321f286c5de86634
Your Name <hassanabubakarmaiwada7@gmail.com>
2025-12-18 21:57:32 +0100

    Fix changing database to supabase database

--- config/db.js
+++ config/db.js
@@ -3,12 +3,8 @@ import doten from 'dotenv'
 doten.config();
 
 export const dbConnection = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
+  process.env.DB_URL,
   {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
     dialect: process.env.DB_DIALECT,
     dialectOptions: {
       ssl: {

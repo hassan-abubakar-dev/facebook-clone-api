408c6f6f6fc6254d61cbd6e0a59c264054b1ede6
Your Name <hassanabubakarmaiwada7@gmail.com>
2025-12-19 22:50:46 +0100

    change database to neon postgres and update the .env and db.js

--- config/cors.js
+++ config/cors.js
@@ -1,6 +1,6 @@
 import cors from 'cors';
 
-const allowOrigins = ['http://localhost:5173', 'https://facebook-clone-frontend-delta.vercel.app', 'https://snazzy-dusk-a9ca96.netlify.app']; 
+const allowOrigins = ['http://localhost:5173', 'https://facebook-clone-frontend-delta.vercel.app', 'https://facebook-clone-frontend-n1503wj1f.vercel.app']; 
 
 const corOptions = cors({
     origin: (origin, cb) => {
